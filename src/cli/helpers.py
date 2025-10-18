ec4663815004ef6423e0cba76418d4898798900d
Navis-Ayara <navisayara@gmail.com>
2025-10-18 18:54:58 +0300

    adds database CRUD helpers

--- src/cli/helpers.py
+++ src/cli/helpers.py
@@ -8,3 +8,43 @@ engine = create_engine("sqlite:///sparks_store.db")
 Session = sessionmaker(bind=engine)
 
 session = Session()
+
+def create_spark(content: str, context: str) -> None:
+    spark = Spark(
+        content=content,
+        context=context
+    )
+
+    session.add(spark)
+    session.commit()
+
+def get_sparks() -> list[Spark]:
+    sparks = session.query(Spark).all()
+
+    return sparks
+
+def get_sparks_by_context(context: str) -> list[Spark]:
+    sparks = session.query(Spark).filter_by(context=context).all()
+
+    return sparks
+
+def edit_spark(spark_id: int, content_edit: str) -> bool:
+    spark = session.query(Spark).where(Spark.id == spark_id).first()
+
+    if spark:
+        spark.content = content_edit
+        session.commit()
+
+        return True
+    return False
+
+def delete_spark(spark_id: int) -> bool:
+    spark = session.query(Spark).where(Spark.id == spark_id).first()
+
+    if spark:
+        session.delete(spark)
+        session.commit()
+
+        return True
+    return False
+    
--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Integer, Text, DateTime, Column, func
 from sqlalchemy.orm import declarative_base
-
 Base = declarative_base()
+
 
 class Spark(Base):
     __tablename__ = 'sparks'
@@ -11,3 +11,4 @@ class Spark(Base):
     context = Column(Text(), nullable=False)
     created_at = Column(DateTime(), server_default=func.now())
     updated_at = Column(DateTime(), onupdate=func.now())
+    
